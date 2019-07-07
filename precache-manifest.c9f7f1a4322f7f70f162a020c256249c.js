ebd83383d7303dea8b3fa025d96cebfa6efba304
Giuseppe Setem <giuseppe7d@gmail.com>
2019-07-07 14:08:35 -0300

    Updates

---
+++ precache-manifest.c9f7f1a4322f7f70f162a020c256249c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b940be3192006c1043e1fe2c6ec722",
+    "url": "/inMaps-Creator/index.html"
+  },
+  {
+    "revision": "2d7564beb5373446091c",
+    "url": "/inMaps-Creator/static/css/2.35043675.chunk.css"
+  },
+  {
+    "revision": "380ced718b2414adbb9a",
+    "url": "/inMaps-Creator/static/css/main.9f174433.chunk.css"
+  },
+  {
+    "revision": "2d7564beb5373446091c",
+    "url": "/inMaps-Creator/static/js/2.ffb8c74a.chunk.js"
+  },
+  {
+    "revision": "380ced718b2414adbb9a",
+    "url": "/inMaps-Creator/static/js/main.58552e44.chunk.js"
+  },
+  {
+    "revision": "ce3b0173bbcae8d64f63",
+    "url": "/inMaps-Creator/static/js/runtime~main.eeb80d12.js"
+  }
+]);
